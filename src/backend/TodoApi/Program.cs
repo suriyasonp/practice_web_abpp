454982c4fc4d037793d3d2fdbfbce745384d52b1
Suriya <suriyasonp.tech@gmail.com>
2026-09-10 13:27:31 +0700

    init sqlite db

--- src/backend/TodoApi/Program.cs
+++ src/backend/TodoApi/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using TodoApi.Dtos;
+using TodoApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
