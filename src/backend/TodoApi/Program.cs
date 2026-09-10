96d66f7405fb35dbad2d8000c65f672c1b905843
Suriya <suriyasonp.tech@gmail.com>
2026-09-10 12:00:27 +0700

    add first endpoint

--- src/backend/TodoApi/Program.cs
+++ src/backend/TodoApi/Program.cs
@@ -14,6 +14,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () => "Hello World!");
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
